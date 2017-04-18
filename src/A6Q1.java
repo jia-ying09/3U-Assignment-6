c26381c07046700ed8f3db80f0f1392cf127e71e
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-04-18 10:15:40 -0400

    First Draft

---
+++ src/A6Q1.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author linj4653
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // add scanner
+        Scanner input = new Scanner(System.in);
+
+        //ask the user how many students are in their class
+        System.out.println("How many students are in the class?");
+
+        //input the number of students that are in the class
+        int students = input.nextInt();
+
+        //ask the user to enter the marks are
+        System.out.println("Enter the marks: ");
+
+        //input the percentage of marks there are per student
+        double[] marks = new double[students];
+
+        //loop for inputting the marks
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = input.nextDouble();
+        }
+
+        //getting the sum of the marks
+        double sum = 0;
+        for (int i = 0; i < marks.length; i++) {
+             sum = sum + marks[i];
+        }
+
+             //leave a blank line
+             System.out.println(" "); 
+
+             //finding the average
+            double average = (sum / students);
+
+            //outputting the class average
+            System.out.println("The class average is " + average + " %");
+
+        }
+    }
---
+++ src/A6Q2.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linj4653
+ */
+public class A6Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // insert scanner
+        Scanner input = new Scanner (System.in);
+        
+        //ask the user how many people are in the group
+        System.out.println("How many people are in the group?");
+        //inputting the amount of people there are in the group
+        int people = input.nextInt();
+        
+        //ask the user to enter the height of each individual
+        System.out.println("Enter the heights in cm: ");
+        
+        
+        double[] height = new double [people];
+        
+        //create a loop for the t
+        for (int i = 0; 1 < height.length; i++){
+       height [i] = input.nextDouble();
+       
+        }
+        //find the average of everyones heights
+        he
+        
+        
+        
+    }
+}
