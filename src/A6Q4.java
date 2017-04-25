0fff4e1970e94c1db7d4b46d87b1cee8cf87d85f
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-04-25 10:10:56 -0400

    first draft

--- src/A6Q4.java
+++ src/A6Q4.java
@@ -26,35 +26,39 @@ public class A6Q4 {
 
         //loop until all 10 numbers are inserted
         for (int i = 0; i < marks.length; i++) {
+
             //insert 10 numbers
             marks[i] = input.nextInt();
         }
 
+        //loop 10 times
         for (int i = 0; i < 10; i++) {
-            {
-                for (int j = marks.length; j < 9; j++) {
+            
+            //loop until all the numbers are in their rightful places
+            for (int j = 0; j < 9; j++) {
 
-                    //put the numbers in order if array 1 is greater
-                    if (marks[j] < marks[j - 1]) {
+                //put the numbers in order if array 1 is greater
+                if (marks[j] < marks[j + 1]) {
 
-                        //make a temporary integer
-                        int temp = marks[j - 1];
-                        //swapping the numbers
-                        marks[j - 1] = marks[j];
-                        //switch the places of array 1
-                        marks[j] = temp;
-                    }
+                    //make a temporary integer
+                    int temp = marks[j + 1];
+                    
+                    //swapping the numbers
+                    marks[j + 1] = marks[j];
+                    //switch the places of array 1
+                    marks[j] = temp;
                 }
             }
-            System.out.println(" ");
+        }
+        
+        //telling the user their numbers
+        System.out.println("Here are the marks in ascending order: ");
+        
+        //order the numbers into ascending order
+        for (int i = 9; i > -1; i--) {
 
-            //order the numbers into ascending order
-            for (int n = 9; n > -9; n--) {
-
-                //output the numbers inascending order
-                System.out.print(marks[i] + ", ");
-            }
-
+            //output the numbers inascending order
+            System.out.print(marks[i]);
         }
     }
 }
---
+++ src/A6Q5.java
@@ -0,0 +1,96 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author linj4653
+ */
+public class A6Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // insert scanner
+        Scanner input = new Scanner(System.in);
+
+        //ask the user how many students are in the class       
+        System.out.println("Enter the number of students in the class: ");
+
+        //input the number of students in the class       
+        int students = input.nextInt();
+
+        //ask the user to enter the marks
+        System.out.println("Enter each individuals percentage: ");
+
+        //input the marks
+        double[] marks = new double[students];
+
+
+        //loop until all marks are inserted
+        for (int i = 0; i < marks.length; i++) {
+
+            //insert the marks
+            marks[i] = input.nextDouble();
+        }
+
+        //loop 10 times
+        for (int i = 0; i < marks.length; i++) {
+
+            //loop until all the numbers are in their rightful places
+            for (int j = 0; j < (marks.length - 1); j++) {
+
+                //put the numbers in order if array 1 is greater
+                if (marks[j] < marks[j + 1]) {
+
+                    //make a temporary integer
+                    double temp = marks[j + 1];
+
+                    //swapping the numbers
+                    marks[j + 1] = marks[j];
+                    //switch the places of array 1
+                    marks[j] = temp;
+                }
+            }
+        }
+
+
+        //telling the user their numbers
+        System.out.println("Here are the marks in ascending order: ");
+
+        //order the numbers into ascending order
+        for (int i = (marks.length - 1); i > -1; i--) {
+
+            //output the numbers inascending order
+            System.out.print(marks[i] + " ");
+        }
+
+        //blank line
+        System.out.println(" ");
+
+        //if the median is an even number
+        if (students % 2 == 0) {
+
+            //find the median of an even number
+            double even = (marks[students / 2] + marks[(students / 2) - 1]) / 2;
+
+            //output the two numbers in the median
+            System.out.println("The two medians are " + marks[students / 2] + " " + marks[(students / 2) - 1]);
+
+            //output the median
+            System.out.println("The class median is " + even);
+
+        } else {
+
+            //finding the median of odd number    
+            double median = marks[students / 2];
+
+            //tell the user their median
+            System.out.println("The class median is " + median + ".");
+        }
+    }
+}
