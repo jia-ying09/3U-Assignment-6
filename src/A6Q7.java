69ef1d76577e102e4c030938dc2abb178ee8ef68
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-04-27 10:15:31 -0400

    First Draft

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -13,10 +13,15 @@ public class A6Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //getting all the prime number drom 2 - 1000
+        int numbers = 2 + 1;
+        int integers = numbers + 1;
+        // make a loop to get all the prime numbers
         for (int i = 2; 1000 > i; i++){
             int prime = 2 + 1;
             
+            //let p equal the first integer
             int p = 2;
             
 
