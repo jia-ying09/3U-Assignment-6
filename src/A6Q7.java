3ee63cce9a11dd031571ce4b95987de05d495b3d
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-04-27 10:09:04 -0400

    First Draft

---
+++ src/A6Q7.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linj4653
+ */
+public class A6Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 2; 1000 > i; i++){
+            int prime = 2 + 1;
+            
+            int p = 2;
+            
+
+        }
+    }
+}
