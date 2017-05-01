fc7762ca91a9c15a3229efcfcdfa45bda08a6368
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-05-01 10:15:19 -0400

    Final Draft

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -13,16 +13,31 @@ public class A6Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+            
         //creating a ist of integers
         int[] numbers = new int[1000];
+        
         {
-            int l = 2;
-            l = l + l;
+            for (int i = 0; i < 1000; i++){
+                numbers[i] = i;
+                
+               //letting p equal 2 
+                int p = 2;}
+                
+            p = p + i;
+                for (int i = 2; i < 1000; i++) 
+                    ||  for (int i = 3; i < 1000; i++){
+                  
+                
+            }
+                
+            }
+           
         }
         //creating a list of integers up to 1000
-        for (int i = 1; i > 1000; i++) {
+       /* for (int i = 1; i > 1000; i++) {
 
-            //letting p equal 2
+            
             int p = 2;
 
             //counting up in increments of p 
@@ -50,8 +65,6 @@ public class A6Q7 {
             } else {
 
                 //stop
-                break;
+                break; */
             }
-        }
-    }
-}
+
